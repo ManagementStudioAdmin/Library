3309fbea51e0a7b402ae1bba0080ce636996c858
ManagementStudioAdmin <api@managementstudio.com>
2021-12-30 14:26:41 +0000

    init draft

---
+++ PowerShell Scripts/Unlock a Self Schedule.ps1
@@ -0,0 +1,19 @@
+#######################  QUICK CONNECTION  ######################
+#Import-Module "c:\ManagementStudio\ManagementStudioApi"
+#Connect-MSApi -ApiUrl "http://ManagementStudio" -ProjectId 1 -UserName "UserName" -Password "Password" -Logfile "C:\ManagementStudio\ScriptLog.txt"
+#################################################################
+
+## Example Input. ($ScriptArgs is provided by MS)
+$ScriptArgs = @{ Items = @(1000, 1001, 1002); Module = 'UserMigrations' }
+
+## Script Start
+#----------------------------------------------------------------
+
+## This script can be used as the target of a Custom Button on the User Grid to unlock the Surveys of the selected Users.
+
+## Unlock the Surveys of a given list of Users
+$surveyIdToUnlock = 99
+Update-MSLockSurveyForModuleItems -Module $ScriptArgs.Module -InstanceIds $ScriptArgs.Items -SurveyId $surveyIdToUnlock -UnLock | Out-Null
+
+## An MSApiResult will show as a notification in the UI to the User that initiated the script
+New-MSApiResult -Header "Survey Unlocked" -Content "$($ScriptArgs.Items.Count) Surveys Unlocked" -Status Success
---
+++ PowerShell Scripts/Unlock a Survey.ps1
@@ -0,0 +1,5 @@
+#######################  QUICK CONNECTION  ######################
+#Import-Module "c:\ManagementStudio\ManagementStudioApi"
+#Connect-MSApi -ApiUrl "http://ManagementStudio" -ProjectId 1 -UserName "UserName" -Password "Password" -Logfile "C:\ManagementStudio\ScriptLog.txt"
+#################################################################
+
